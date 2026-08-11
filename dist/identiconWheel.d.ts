d4993b5955e4c68445529025ddbe062e7ad5ca84
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-08-11 10:20:29 +0100

    Build identicon renderer entry declarations

---
+++ dist/identiconWheel.d.ts
@@ -0,0 +1,3 @@
+export { renderAstralIdenticonV8 } from "./identiconV8.js";
+export { renderAstralIdenticonV9 } from "./identiconV9.js";
+export type { AstralIdenticonAssetSource, AstralIdenticonInput, AstralIdenticonPalette, AstralIdenticonPlanetState, AstralIdenticonTracedGlyph, AstralIdenticonV8NorthStar, AstralIdenticonV8RecoveryStar, AstralIdenticonV8Request, AstralIdenticonV9Request, } from "./identiconTypes.js";
