6e4ffe6c9752ade9196c92a217142152a5031c8d
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-08-11 10:20:13 +0100

    Add v8 identicon renderer declarations

---
+++ dist/identiconV8.d.ts
@@ -0,0 +1,2 @@
+import type { AstralIdenticonAssetSource, AstralIdenticonV8Request } from "./identiconTypes.js";
+export declare function renderAstralIdenticonV8(request: AstralIdenticonV8Request, assets: AstralIdenticonAssetSource): Promise<string>;
